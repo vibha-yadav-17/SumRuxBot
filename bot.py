e5eebc7bb4d681d22aade21296374e451bda561a
Vibha Yadav <vyadav477@gmail.com>
2020-05-22 23:41:49 +0530

    changes

--- bot.py
+++ bot.py
@@ -1,111 +1,146 @@
-import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler,InlineQueryHandler
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputTextMessageContent
-import os
-PORT = int(os.environ.get('PORT', 5000))
 
-# Enable logging
+
+"""
+First, a few callback functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          ConversationHandler)
+
+#Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = '1081133046:AAHOOqRbAC7L-QuJoDP6btiIQ2qHnXcX8_0'
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-# def start(update, context):
-   #  """Send a message when the command /start is issued."""
-    # update.message.reply_text('Hi! Welcome to SumRuxBookExchange. Do you Need Books or have books'
-    
-    # )
+INFO, LOCATION, BIO, CLASSNAMES = range(4)
 
 
 def start(update, context):
-    update.message.reply_text('Hi! Welcome to SumRuxBookExchange. Do you Need Books or have books?')
-    keyboard = [[InlineKeyboardButton("Need", callback_data='1'),
-                 InlineKeyboardButton("Have", callback_data='2')],
-    ]
+    reply_keyboard = [['Need', 'Have']]
 
-    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text(
+        'Hi! Welcome to SumRuxBookExchange. I will hold a conversation with you. '
+        'Send /cancel to stop talking to me.\n\n'
+        'Do you need book or have book?',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
-   
-
-def button(update, context):
-    query = update.callback_query
-
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
-    query.answer()
-
-    query.edit_message_text(text="Selected option: {}".format(query.data))
+    return INFO
 
 
-    
+def info(update, context):
+    user = update.message.from_user
+    logger.info("Requirement of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('Please enter your location,'
+                              'or send /skip if you don\'t want to.',
+                              reply_markup=ReplyKeyboardRemove())
 
-def inline_caps(update, context):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = list()
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    context.bot.answer_inline_query(update.inline_query.id, results)
-    
+    return LOCATION
 
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('What help u need!')
-    
 
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+
+def location(update, context):
+    user = update.message.from_user
+    user_location = update.message.location
+    logger.info("Location of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('Please enter your name!')
+
+    return BIO
+
+
+def skip_location(update, context):
+    user = update.message.from_user
+    logger.info("User %s did not send a location.", user.first_name)
+    update.message.reply_text('You seem a bit paranoid! '
+    'At last, tell me something about yourself.')
+
+    return BIO
+
+
+def bio(update, context):
+    user = update.message.from_user
+    logger.info("Bio of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('Thank you! What class/standard book do you want?')
+
+    return CLASSNAMES
+
+def classnames(update, context):
+    user = update.message.from_user
+    logger.info("Classname of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('Thank you! I hope we can talk again.')
+
+
+
+    return ConversationHandler.END
+
+
+def cancel(update, context):
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text('Bye! I hope we can talk again some day.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
 
 
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+
 def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
+    # will Create the Updater and pass it our bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater(TOKEN, use_context=True)
-    inline_caps_handler = InlineQueryHandler(inline_caps)
+    updater = Updater("TOKEN", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CallbackQueryHandler(button))
+    # Add conversation handler with the states INFO, LOCATION, BIO, CLASSNAMES
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
 
-    dp.add_handler(inline_caps_handler)    
+        states={
+            INFO: [MessageHandler(Filters.regex('^(Need|Have)$'), info)],
 
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+            #PHOTO: [MessageHandler(Filters.photo, photo),
+             #       CommandHandler('skip', skip_photo)],
+
+            LOCATION: [MessageHandler(Filters.text, location),
+                       CommandHandler('skip', skip_location)],
+
+            BIO: [MessageHandler(Filters.text, bio)],
+
+            CLASSNAMES:[MessageHandler(Filters.text, classnames)],
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    dp.add_handler(conv_handler)
 
     # log all errors
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://desolate-brushlands-09341.herokuapp.com/' + TOKEN)
+    updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+#ctrl c for stopping the bot
+    # SIGTERM or SIGABRT. This should be used most of the time
+    # start_polling() is non-blocking and will stop the bot.
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
