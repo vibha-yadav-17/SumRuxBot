edd587a4c0b4e900e69248c567df082ae46eea68
Seditious Lassie <62759592+seditiouslassie@users.noreply.github.com>
2020-05-26 15:07:40 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -24,85 +24,114 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-INFO, LOCATION, BIO, CLASSNAMES = range(4)
+START, LOCALITY,CITY,PINCODE, REQ, BOARD, MEDIUM, SUBJECTS, NUMBER, EMAIL, END = range(11)
 
 
-def start(update, context):
-	update.message.from_user
-	logger.info("City of %s: %s", user.first_name, update.message.text)
+"""def start(update, context):
+	user=update.message.from_user
+	logger.info("locality of%s: %s", user.first_name, update.message.text)
 	update.message.reply_text(
         'Hi! Welcome to SumRuxBookExchange. We at Sumrux identify people in the same pincode and exchange books.' 
         'We help create a community of readers who are physically proximate to each other.'
-        'Which city do you live in?',
+        'WhAT is your locality',
         reply_markup=ReplyKeyboardRemove())
 
-	return CITY
+	return LOCALITY"""
+
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text( 'Hi! Welcome to SumRuxBookExchange. '
+    'Thank you for choosing us to help you'
+    'This is a free service by Sumrux for academic books for covid-19 recovery .'
+    'Let us know the details of the books you are looking for and the books you have.\n'
+    'Please let us know your current locality?')
+
+    return CITY
+
+def city(update, context):
+	user=update.message.from_user
+	logger.info("locality of%s: %s", user.first_name, update.message.text)
+	update.message.reply_text(
+        'What is your city?',
+        reply_markup=ReplyKeyboardRemove())
+
+	return PINCODE
 
 def pincode(update, context):
 	user=update.message.from_user
-	logger.info("Pincode of %s: %s", user.first_name, update.message.text)
+	logger.info("City of %s: %s", user.first_name, update.message.text)
 	update.message.reply_text(
 		'Thankyou, you have made it easier for us to find you a match'
 		'We do need some more information to help you find a suitable bookmatch.'
 		'What is your pincode?',
 		reply_markup=ReplyKeyboardRemove())
 
-	return PINCODE 
+	return REQ
 
-def standard(update, context):
+def req(update, context):
+	reply_keyboard = [['Need' , 'Have']]
+
 	user=update.message.from_user
-	logger.info("Books of Standard %s: %s", user.first_name, update.message.text)
+	logger.info("Pincode of %s: %s", user.first_name, update.message.text)
 	update.message.reply_text(
-		'Thankyou for trusting us with your information.'
-		'We are on our way to connect you with other readers around you.'
-		'Which standard books are you looking for?',
-		 reply_markup=ReplyKeyboardRemove())
+    	'Do you need book or have book?',
+    	 reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))	
 
-	return STANDARD
+	return BOARD
+
 
 def board(update, context):
 	user=update.message.from_user
-	logger.info("Books of Board %s: %s", user.first_name, update.message.text)
+	logger.info("Requirement of %s: %s", user.first_name, update.message.text)
 	update.message.reply_text(
-		'Almost there. Do you have any specific board in mind?',
+		'Almost there. Can you tell us the board and standard of the books?',
 		 reply_markup=ReplyKeyboardRemove())
 
-	return BOARD
+	return MEDIUM
 
 def medium(update, context):
 	reply_keyboard = [['English' , 'Hindi']]
 
 	user=update.message.from_user
-	logger.info("Books of Medium %s: %s", user.first_name, update.message.text)
+	logger.info("Books of Board %s: %s, Books of Standard %s: %S", user.first_name, update.message.text)
 	update.message.reply_text(
     	'Do you want English Medium or Hindi Medium Books?',
     	 reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
-	return MEDIUM
+	return SUBJECTS
 
 def subjects(update, context):
 	user=update.message.from_user
-	logger.info("Books of Subject %s: %s", user.first_name, update.message.text)
+	logger.info("Books of Medium %s: %s", user.first_name, update.message.text)
 	update.message.reply_text(
-		 'One last thing. Which subjects are you look for?',
+		 'One last thing. Which subjects are you looking for?',
 		 reply_markup=ReplyKeyboardRemove())
 
-	return SUBJECTS
+	return NUMBER
+
+def number (update,context):
+	user=update.message.from_user
+	logger.info("Phone Number of %s: %s", user.first_name, update.message.text)
+	update.message.reply_text(
+		'Could you please share with us your phone number?',
+		reply_markup=ReplyKeyboardRemove())
+
+	return EMAIL
 
 def email(update, context):
 	user=update.message.from_user
-	logger.info("Email of %s; %s", user.first_name, update.message.text)
+	logger.info("Books of Subject%s: %s", user.first_name, update.message.text)
 	update.message.reply_text(
 		'We are glad you are trusting us with your information'
 		'If you could give us your email ID, it would help us send you the relevant information'
 		'What is your email?', 
 		reply_markup=ReplyKeyboardRemove())
 
-	return EMAIL 
+	return END
 
 def end(update,context):
 	user=update.message.from_user
-	logger.info("User %s ended the conversation.", user.first_name)
+	logger.info("Email of%s: %s", user.first_name,update.message.text)
 	update.message.reply_text('I hope we are of help to you. Happy reading!',
 		                       reply_markup=ReplyKeyboardRemove())
 
@@ -111,7 +140,7 @@ def end(update,context):
 
 def cancel(update, context):
     user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
+    logger.info("User %s ended the conversation.", user.first_name)
     update.message.reply_text('Bye! I hope we can talk again some day.',
                               reply_markup=ReplyKeyboardRemove())
 
@@ -126,7 +155,7 @@ def error(update, context):
 def main():
     # will Create the Updater and pass it our bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
-    updater = Updater("TOKEN", use_context=True)
+    updater = Updater("1099113463:AAHBgI02WQbWvBqHQQVc-NBXI_MXLGIWBBA", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -136,17 +165,28 @@ def main():
         entry_points=[CommandHandler('start', start)],
 
         states={
-            INFO: [MessageHandler(Filters.regex('^(Need|Have)$'), info)],
+
+ 		    #LOCALITY: [MessageHandler(Filters.text, locality)],
+            CITY: [MessageHandler(Filters.text, city)],
+           
 
             #PHOTO: [MessageHandler(Filters.photo, photo),
              # CommandHandler('skip', skip_photo)],
+            PINCODE: [MessageHandler(Filters.text, pincode)],
+              REQ: [MessageHandler(Filters.text, req)],
 
-            LOCATION: [MessageHandler(Filters.text, location),
-                       CommandHandler('skip', skip_location)],
+            BOARD: [MessageHandler(Filters.text, board)],
+          
 
-            BIO: [MessageHandler(Filters.text, bio)],
+            MEDIUM:[MessageHandler(Filters.text, medium)],
 
-            CLASSNAMES:[MessageHandler(Filters.text, classnames)],
+            SUBJECTS: [MessageHandler(Filters.text, subjects)],
+
+            NUMBER: [MessageHandler(Filters.text, number)],
+
+            EMAIL: [MessageHandler(Filters.text, email)],
+            END: [MessageHandler(Filters.text,end)]
+
         },
 
         fallbacks=[CommandHandler('cancel', cancel)]
